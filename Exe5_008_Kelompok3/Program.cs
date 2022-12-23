526214793c8bb678e6ed001c758db1c3b5c9dcd5
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-12-23 16:29:47 +0700

    Create class QNode, then create variable

--- Exe5_008_Kelompok3/Program.cs
+++ Exe5_008_Kelompok3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace Exe5
+{
+    // A linked list (LL) node to store a queue entry
+    class QNode
+    {
+        // Create variable key and next
+        public string key;
+        public QNode next;
+        // constructor to create a new linked list node
+       
+    }
+   
+   
+    
+}
